1967ed111301fc20f1da0a3e27ef53e0355ef694
MadRobin1339 <114031799+MadRobin1339@users.noreply.github.com>
2024-01-13 11:47:23 -0500

    Started the subsystem
    Started making the subsystem. Not much done.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -13,6 +13,9 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
+
+  public static final int CLIMBER_LEAD_CAN_ID = 1;
+
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
