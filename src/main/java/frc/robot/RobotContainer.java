53e2f58f613c90722569e6014a9809715902f733
blaze <Arnnav0kudale@gmail.com>
2024-01-21 17:52:13 -0500

    fixed error in robotcontainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,7 +6,6 @@ package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.autos.Autos;
-import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ArmChassisPivotSubsystem;
 import frc.robot.subsystems.ExampleSubsystem;
 
@@ -51,8 +50,6 @@ public class RobotContainer {
    */
   private void configureBindings() {
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
 
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
