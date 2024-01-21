84e188619bae1ed9d44f7e192b4fa8c53c0475fd
blaze <Arnnav0kudale@gmail.com>
2024-01-21 17:43:32 -0500

    assemblyhomecmd almost finished NEED ACP SUBSYSTEM

---
+++ src/main/java/frc/robot/commands/assembly/AssemblyHomePositionCommand.java
@@ -0,0 +1,36 @@
+package frc.robot.commands.assembly;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants;
+import frc.robot.commands.ShintakePivot.STPGoToPositionCommand;
+import frc.robot.commands.ArmChassisPivot.ACPGoToPositionCommand;
+import frc.robot.subsystems.LEDSubsystem;
+import frc.robot.subsystems.ShintakePivotSubsystem;
+import frc.robot.subsystems.ArmChassisPivotSubsystem;
+import frc.robot.subsystems.ArmChassisPivotSubsystem.ArmShintakeMessenger;
+import frc.robot.util.StateMachine;
+
+public class AssemblyHomePositionCommand extends SequentialCommandGroup {
+
+    public AssemblyHomePositionCommand(ArmChassisPivotSubsystem ACPSubsystem, ArmShintakeMessenger armShintakeMessenger,
+            ShintakePivotSubsystem STPSubsystem, LEDSubsystem ledSubsystem, StateMachine sm) {
+        addCommands(
+            new InstantCommand(() -> ACPSubsystem.setInIntakePosition(false)),
+            new InstantCommand(ledSubsystem::setLEDDisable),
+
+            // Command 1
+            new STPGoToPositionCommand(STPSubsystem, Constants.HOME_POSITION_STP)
+                .alongWith(new InstantCommand(() -> SmartDashboard.putString("Homing stage", "STAGE 2"))),
+
+            // Command 2
+            new ACPGoToPositionCommand(ACPSubsystem, Constants.HOME_POSITION_ACP)
+                .alongWith(new InstantCommand(() -> SmartDashboard.putString("Homing stage", "STAGE 3"))),
+
+            new InstantCommand(ledSubsystem::setLEDEnable),
+            new InstantCommand(() -> SmartDashboard.putString("Homing stage", "DONE")),
+            new InstantCommand(() -> sm.setAtHome(true))
+        );
+    }
+}
