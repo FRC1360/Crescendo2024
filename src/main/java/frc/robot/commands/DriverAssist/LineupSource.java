256b14b6313dc7ddf215ecfe82af11e864050406
blaze <Arnnav0kudale@gmail.com>
2024-01-22 17:26:45 -0500

    proper indentation

--- src/main/java/frc/robot/commands/DriverAssist/LineupSource.java
+++ src/main/java/frc/robot/commands/DriverAssist/LineupSource.java
@@ -8,24 +8,24 @@ public class LineupSource extends Command {
     public LineupSource() {}
 
   // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void initialize() {
-    double x = 1.24;  // TODO TUNE THESE VALUES
-    double y = 1.70;  // TODO TUNE THESE VALUES
-    double r = 120.0; // TODO TUNE THESE VALUES
+    @Override
+    public void initialize() {
+        double x = 1.24;  // TODO TUNE THESE VALUES
+        double y = 1.70;  // TODO TUNE THESE VALUES
+        double r = 120.0; // TODO TUNE THESE VALUES
 
-    Command pathfindAuto = new PathfindAuto(new Pose2d(x, y, Rotation2d.fromDegrees(r))).getCommand();
-    pathfindAuto.schedule();
-  } 
+        Command pathfindAuto = new PathfindAuto(new Pose2d(x, y, Rotation2d.fromDegrees(r))).getCommand();
+        pathfindAuto.schedule();
+    } 
 
-  @Override
-  public void execute() {}
+    @Override
+    public void execute() {}
 
-  @Override
-  public void end(boolean interrupted) {}
+    @Override
+    public void end(boolean interrupted) {}
 
-  @Override
-  public boolean isFinished() {
-    return true;
-  }
+    @Override
+    public boolean isFinished() {
+        return true;
+    }
 }
