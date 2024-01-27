a38f1626a4583cdbcc3287bda0d7a2629944b4b1
PerfectlyInternal <55606714+PerfectlyInternal@users.noreply.github.com>
2024-01-27 09:52:54 -0500

    junk imports

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -52,13 +52,6 @@ public class RobotContainer {
   private void configureBindings() {
 
     left_controller.button(1).onTrue(intakeCommand);
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    // new Trigger(m_exampleSubsystem::exampleCondition)
-    //     .onTrue(new ExampleCommand(m_exampleSubsystem));
-
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    //m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
 
     swerveSubsystem.setDefaultCommand(new DefaultDriveCommand(
         swerveSubsystem,
