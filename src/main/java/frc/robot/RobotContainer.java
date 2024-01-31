3e3b622afdbb8468f9d4385ba83232a8a504252d
blaze <Arnnav0kudale@gmail.com>
2024-01-30 20:37:28 -0500

    Revert "Merge branch 'test_commands' into Intake/Shooter"
    This reverts commit c37f7185d1f35a9bfc3f4482a8dce4fb6cf10421, reversing
    changes made to ed1d2e5d4f5b0da1fd2f8130ed530e1476b6221e.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,20 +4,11 @@
 
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
-
-import java.util.function.DoubleSupplier;
-import java.util.function.BooleanSupplier;
-
 import frc.robot.commands.DefaultDriveCommand;
-import frc.robot.commands.Shintake.DefaultShintakeCommand;
-import frc.robot.commands.Shintake.ShootSpeakerCommand;
 import frc.robot.commands.Shintake.IntakeCommand;
-
+import frc.robot.commands.Shintake.ShootSpeakerCommand;
 import frc.robot.subsystems.ShintakeSubsystem;
 import frc.robot.subsystems.SwerveSubsystem;
-import frc.robot.subsystems.ArmChassisPivotSubsystem;
-
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -33,10 +24,6 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ShintakeSubsystem shintakeSubsystem = new ShintakeSubsystem();
   // public final SwerveSubsystem swerveSubsystem = new SwerveSubsystem();
-  private final ShintakeSubsystem m_shintakeSubsystem = new ShintakeSubsystem();
-
-  //what is this supposed to be :(
-  public final ArmChassisPivotSubsystem ACPSubsystem = new ArmChassisPivotSubsystem(() -> 0.0, () -> false);
 
   private final IntakeCommand intakeCommand = new IntakeCommand(shintakeSubsystem);
   // Replace with CommandPS4Controller or CommandJoystick if needed
@@ -63,15 +50,6 @@ public class RobotContainer {
 
     left_controller.button(1).whileTrue(new IntakeCommand(shintakeSubsystem));
     right_controller.button(1).whileTrue(new ShootSpeakerCommand(shintakeSubsystem));
-    left_controller.button(7).onTrue(new IntakeCommand(m_shintakeSubsystem));
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-
-    // new Trigger(m_exampleSubsystem::exampleCondition)
-    //     .onTrue(new ExampleCommand(m_exampleSubsystem));
-
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    //m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
 
     //swerveSubsystem.setDefaultCommand(new DefaultDriveCommand(
         //swerveSubsystem,
@@ -88,7 +66,7 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return null;
+    return null;//Autos.exampleAuto(m_exampleSubsystem);
   }
 
   private static double deadband(double value, double deadband) {
