8d722fb101d04ca3742f33a6c0c275923e56bfb3
PerfectlyInternal <55606714+PerfectlyInternal@users.noreply.github.com>
2024-02-03 14:16:29 -0500

    add some debug for targeting

--- src/main/java/frc/robot/autos/PathfindAuto.java
+++ src/main/java/frc/robot/autos/PathfindAuto.java
@@ -1,10 +1,14 @@
 package frc.robot.autos;
 
+import java.lang.reflect.Field;
+
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.commands.PathfindingCommand;
 import com.pathplanner.lib.path.PathConstraints;
 
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.Constants;
@@ -16,10 +20,12 @@ public class PathfindAuto {
 
     private SwerveSubsystem swerveSubsystem;
     private Pose2d targetPose; 
-    private PathConstraints constraints; 
+    private PathConstraints constraints;
+    private Field2d target = new Field2d();
     
     public PathfindAuto(SwerveSubsystem swerveSubsystem, Pose2d targetPose) { 
         this.targetPose = targetPose; 
+        target.setRobotPose(targetPose);
 
         this.swerveSubsystem = swerveSubsystem; 
 
@@ -33,6 +39,7 @@ public class PathfindAuto {
         return AutoBuilder.pathfindToPose(this.targetPose, constraints, 0.0, 0.5)
         .until(() -> swerveSubsystem.isInRange(targetPose, AutoConstants.positionTolerance * 20, AutoConstants.angleTolerance * 10))
         .alongWith(new InstantCommand(() -> System.out.println(this.targetPose)))
+        .alongWith(new InstantCommand(() -> SmartDashboard.putData("Target pose", this.target)))
         .andThen(new AlignToPose(this.swerveSubsystem, targetPose));
     }
 }
