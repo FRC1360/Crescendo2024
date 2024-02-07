ae5b63a14312c859dd535fbf79cb44117092d6bd
PerfectlyInternal <55606714+PerfectlyInternal@users.noreply.github.com>
2024-02-06 19:53:31 -0500

    instal URCL, cleanup stuff

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,19 +8,13 @@ import frc.robot.autos.FetchPath;
 import frc.robot.autos.PathfindAuto;
 
 import frc.robot.commands.DefaultDriveCommand;
-// import frc.robot.commands.shintake.DefaultShintakeCommand;
-// import frc.robot.commands.shintake.IntakeCommand;
 import frc.robot.subsystems.ExampleSubsystem;
-import frc.robot.subsystems.ShintakeSubsystem;
 import frc.robot.subsystems.SwerveSubsystem;
 
 import java.util.ArrayList;
 
 import com.pathplanner.lib.auto.AutoBuilder;
-import com.pathplanner.lib.commands.FollowPathCommand;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -38,11 +32,6 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  //private final ShintakeSubsystem m_shintakeSubsystem = new ShintakeSubsystem();
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-
-  //private final DefaultShintakeCommand m_defaultShintakeCommand = new DefaultShintakeCommand(m_shintakeSubsystem);
-  
   private final CommandJoystick left_controller = new CommandJoystick(0);
   private final CommandJoystick right_controller = new CommandJoystick(1);
 
@@ -54,12 +43,9 @@ public class RobotContainer {
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-
     this.swerveSubsystem = new SwerveSubsystem(); 
 
     swerveSubsystem.configureAutoBuilder(); // needs to be called everytime robotInits so alliance is updated
-
-    //m_shintakeSubsystem.setDefaultCommand(m_defaultShintakeCommand);
 
     // Configure the trigger bindings
     configureBindings();
@@ -90,9 +76,6 @@ public class RobotContainer {
    * joysticks}.
    */
   private void configureBindings() {
-
-    //left_controller.button(7).onTrue(new IntakeCommand(m_shintakeSubsystem));
-
     swerveSubsystem.setDefaultCommand(new DefaultDriveCommand(
         swerveSubsystem,
         () -> -modifyAxis(left_controller.getY()) * Constants.ROBOT_MAX_VELOCITY_METERS_PER_SECOND, // Modify axis also for alliance color
@@ -108,8 +91,6 @@ public class RobotContainer {
     left_controller.button(5).whileTrue(new PathfindAuto(swerveSubsystem, AlignmentConstants.BLUE_STAGE_RIGHT).getCommand());
 
     left_controller.button(7).onTrue(new InstantCommand(swerveSubsystem::brake)); 
-
-    
   }
 
 
