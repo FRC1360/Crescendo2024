571797cbb210df11e7b7863ef13b7adda20e30e4
RedRock5645 <113565541+RedRock5645@users.noreply.github.com>
2024-02-08 20:23:00 -0500

    added test thing where shintake pivot moves in teleop

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,6 +7,8 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.commands.STPGoToPositionCommand.STPGoToPositionCommand;
+import frc.robot.subsystems.ShintakePivotSubsystem;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -18,6 +20,7 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
+  private ShintakePivotSubsystem m_ShintakePivotSubsystem;
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -82,7 +85,7 @@ public class Robot extends TimedRobot {
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
-    
+    new STPGoToPositionCommand(m_ShintakePivotSubsystem, Constants.STPConstants.NOTE_SCORE_AMP_POSITION_STP);
   }
 
   @Override
