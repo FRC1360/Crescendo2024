48e910015a5ae0514e99e9885f210202f8c5b2f9
MadRobin1339 <114031799+MadRobin13@users.noreply.github.com>
2024-02-17 15:30:52 -0500

    Fixed PID Part 2
    Need this in original commit too. More stuff in the subsystem for PID.

--- src/main/java/frc/robot/subsystems/ShintakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShintakeSubsystem.java
@@ -5,16 +5,20 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkBase;
 import com.revrobotics.CANSparkFlex;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.REVLibError;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkPIDController;
 import com.revrobotics.CANSparkBase.ControlType;
+import com.revrobotics.CANSparkBase.IdleMode;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.wpilibj.Counter;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Preferences;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -22,8 +26,8 @@ import frc.robot.util.TuningTable;
 
 public class ShintakeSubsystem extends SubsystemBase {
 
-  private CANSparkMax m_left;
-  private CANSparkMax m_right;
+  private CANSparkFlex m_left;
+  private CANSparkFlex m_right;
   private RelativeEncoder m_encoderLeft;
   private RelativeEncoder m_encoderRight;
   private CANSparkMax m_back;
@@ -42,16 +46,31 @@ public class ShintakeSubsystem extends SubsystemBase {
   private final TuningTable kD_Lower = new TuningTable("D_Lower_Shooter");
   private final TuningTable kF_Lower = new TuningTable("F_Lower_Shooter");
 
+  private double leftkP, leftkI, leftkD, rightkI, rightkP, rightkD;
+
   public ShintakeSubsystem() {
       //Using CANSparkFlexes for the two shooter neo vortexes
-      this.m_left = new CANSparkMax(Constants.ShintakeConstants.LEFT_SHOOTAKE_CAN_ID, MotorType.kBrushless);
-      this.m_right = new CANSparkMax(Constants.ShintakeConstants.RIGHT_SHOOTAKE_CAN_ID, MotorType.kBrushless);
+      this.m_left = new CANSparkFlex(Constants.ShintakeConstants.LEFT_SHOOTAKE_CAN_ID, MotorType.kBrushless);
+      this.m_right = new CANSparkFlex(Constants.ShintakeConstants.RIGHT_SHOOTAKE_CAN_ID, MotorType.kBrushless);
       this.m_back = new CANSparkMax(Constants.ShintakeConstants.BACK_SHOOTAKE_ID, MotorType.kBrushless);
+      this.m_back.setIdleMode(IdleMode.kBrake);
       this.m_digital = new DigitalInput(Constants.ShintakeConstants.SHINTAKE_SENSOR_PIN);
       this.m_counter = new Counter(m_digital);
       this.m_encoderLeft = m_left.getEncoder();
       this.m_encoderRight = m_right.getEncoder();
+      this.leftkP = 0.0;
+      this.leftkI = 0.000;
+      this.leftkD = 0.00;
+      this.rightkP = 0.00;
+      this.rightkI = 0.0;
+      this.rightkD = 0.00;
 
+    Preferences.initDouble("right kP", rightkP);
+    Preferences.initDouble("right kI", rightkI);
+    Preferences.initDouble("right kD", rightkD);
+    Preferences.initDouble("left kP", leftkP);
+    Preferences.initDouble("left kI", leftkI);
+    Preferences.initDouble("left kD", leftkD);
       // kP_Upper.setDefault(Constants.Shooter.kP_Upper);
       // kI_Upper.setDefault(Constants.Shooter.kI_Upper);
       // kD_Upper.setDefault(Constants.Shooter.kD_Upper);
@@ -67,33 +86,45 @@ public class ShintakeSubsystem extends SubsystemBase {
       this.leftWheelPID = m_left.getPIDController();
 
       
-      leftWheelPID.setP(0.0001); // 0.00015 (3 zeros, 1 one, 1 five)
+      leftWheelPID.setP(0.0008); // 0.0011 (3 zeros, 1 one, 1 five)
       // leftWheelPID.setI(kI_Lower.get());
       // leftWheelPID.setD(0.0005); //0.00012
-      leftWheelPID.setFF(.0002); //0.0001815 (3 zeros, 1 one, 1 eight, 1 one, 1 five)
+      leftWheelPID.setFF(0.00016); //0.0001815 (3 zeros, 1 one, 1 eight, 1 one, 1 five)
 
-      rightWheelPID.setP(0.0001); //0.00003 (4 zeros, 1 three)
+      rightWheelPID.setP(0.0008); //0.0009 (4 zeros, 1 three)
       // rightWheelPID.setI(kI_Upper.get());
       // rightWheelPID.setD(0.0005); // 0.0005 (3 zeros, 1 five)
-      rightWheelPID.setFF(0.0002); //0.0001807 (3 zeros, 1 one, 1 eight, 1 zero, 1 seven)
+      rightWheelPID.setFF(0.00016); //0.0001807 (3 zeros, 1 one, 1 eight, 1 zero, 1 seven)
 
 
       m_left.setInverted(false);
       m_right.setInverted(true);
-      m_back.setInverted(true);
+      m_back.setInverted(false);
+    }
+
+    public boolean isSwitchSet() {
+        return m_counter.get() > 0;
+    }
+
+    public double getBackEncoder() { 
+      return m_back.getEncoder().getPosition(); 
+    }
+
+    public void initializeCounter() {
+        m_counter.reset();
     }
 
     public void setVelocity(double rightVelocity, double leftVelocity) {
         this.leftVelocity = leftVelocity;
         this.rightVelocity = rightVelocity;
 
-        leftWheelPID.setReference(leftVelocity, ControlType.kVelocity);
-        rightWheelPID.setReference(rightVelocity, ControlType.kVelocity);
+        leftWheelPID.setReference(this.leftVelocity, CANSparkFlex.ControlType.kVelocity);
+        rightWheelPID.setReference(this.rightVelocity, CANSparkFlex.ControlType.kVelocity);
 
     }
 
     public boolean shooterWheelsReady() {
-      return (this.leftVelocity != 0 && this.rightVelocity != 0)
+      return getVelocityLeft() != 0 && getVelocityRight() != 0
           && Math.abs(getVelocityLeft() - this.leftVelocity) <= 100
           && Math.abs(getVelocityRight() - this.rightVelocity) <= 100;
   }
@@ -120,11 +151,15 @@ public class ShintakeSubsystem extends SubsystemBase {
   public void varIntake(double backSpeed) {
     m_back.set(backSpeed);
   }
+  
+  public void varFix(double backSpeed) {
+    m_back.set(-backSpeed);
+  }
 
   public void varShoot(double speed) {
     //Set one to negative to side
     m_left.set(speed);
-    m_right.set(-speed);
+    m_right.set(speed);
   }
 
   public boolean getDigitalInput() {
@@ -145,5 +180,35 @@ public class ShintakeSubsystem extends SubsystemBase {
     SmartDashboard.putBoolean("intake sensor state", m_digital.get());
     SmartDashboard.putNumber("left velocity", m_encoderLeft.getVelocity());
     SmartDashboard.putNumber("right velocity", m_encoderRight.getVelocity());
+    SmartDashboard.putNumber("Target left Wheel Velocity", Constants.ShintakeConstants.TARGET_SHOOT_VELOCITY_SPEAKER);
+    SmartDashboard.putNumber("Target right Wheel Velocity", Constants.ShintakeConstants.TARGET_SHOOT_VELOCITY_SPEAKER);
+    //SmartDashboard.putNumber("left Wheel kp", kp);
+    //leftWheelPID.setP()
+    // SmartDashboard.getNumber("right kP", rightkP);
+    // rightWheelPID.setP(rightkP);
+    // SmartDashboard.getNumber("right kI", rightkI);
+    // rightWheelPID.setI(rightkI);
+    // SmartDashboard.getNumber("right kD", rightkD);
+    // rightWheelPID.setD(rightkD);
+    // SmartDashboard.getNumber("left kP", leftkP);
+    // leftWheelPID.setP(leftkP);
+    // SmartDashboard.getNumber("left kI", leftkI);
+    // leftWheelPID.setI(leftkI);
+    // SmartDashboard.getNumber("left kD", leftkD);
+    // leftWheelPID.setD(leftkD);
+    // Preferences.getDouble("right kP", rightkP);
+    // rightWheelPID.setP(rightkP);
+    // Preferences.getDouble("right kI", rightkI);
+    // rightWheelPID.setI(rightkI);
+    // Preferences.getDouble("right kD", rightkD);
+    // rightWheelPID.setD(rightkD);
+    // Preferences.getDouble("left kP", leftkP);
+    // leftWheelPID.setP(leftkP);
+    // Preferences.getDouble("left kI", leftkI);
+    // leftWheelPID.setI(leftkI);
+    // Preferences.getDouble("left kD", leftkD);
+    // leftWheelPID.setD(leftkD);
+    
+
   }
 }
