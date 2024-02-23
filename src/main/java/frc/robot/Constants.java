fbefa9bb92f3d7504ae2a9910e7ee1812e512cf8
GabbiIpwanshek <147567918+GabbiIpwanshek@users.noreply.github.com>
2024-02-23 11:25:44 -0500

    constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -130,19 +130,9 @@ public final class Constants {
     public static final double SOURCE_POSITION_STP = 180;
     public static final double SOURCE_POSITION_ACP = 50;
 
-	// CLIMB_POSITION fix this
-		public static final double CLIMB_POSITION_STP = 180.0; 
-		public static final double CLIMB_POSITION_ACP = 90.0; 
-
-    public static class ShintakeConstants {
-        public static final int INTAKE_SPEED_BACK = 70;
-        public static final int DEFAULT_INTAKE_SPEED = 50;
-        public static final int SHOOT_SPEED_FRONT = 100;
-        public static final int SHOOT_SPEED_BACK = 50;
-        public static final int RIGHT_SHOOTAKE_CAN_ID = 38;
-        public static final int LEFT_SHOOTAKE_CAN_ID = 37;
-        public static final int BACK_SHOOTAKE_ID = 2;
-    }
+    // CLIMB_POSITION fix this
+    public static final double CLIMB_POSITION_STP = 180.0;
+    public static final double CLIMB_POSITION_ACP = 90.0;
 
     public static class ACPConstants {
         // SHOULDER
