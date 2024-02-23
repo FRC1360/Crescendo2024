f524bca5690fcc2d0b357345a3664276074156a1
RedRock5645 <113565541+RedRock5645@users.noreply.github.com>
2024-02-22 20:58:36 -0500

    Merged stuff
    plsdontbreakplsdontbreakplsdontbreak

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Crescendo2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 189;
-  public static final String GIT_SHA = "0880d0634ae0b0d9a1f895945c47368ef01cf100";
-  public static final String GIT_DATE = "2024-02-22 20:21:45 EST";
+  public static final int GIT_REVISION = 99;
+  public static final String GIT_SHA = "78274305bf922c5ac7d4308224d8850c2ef87d08";
+  public static final String GIT_DATE = "2024-02-22 20:41:37 EST";
   public static final String GIT_BRANCH = "assembly_commands";
-  public static final String BUILD_DATE = "2024-02-22 20:35:07 EST";
-  public static final long BUILD_UNIX_TIME = 1708652107257L;
+  public static final String BUILD_DATE = "2024-02-22 20:52:30 EST";
+  public static final long BUILD_UNIX_TIME = 1708653150936L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
