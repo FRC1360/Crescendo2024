5d8e1af73622178224df0605fb47b1611066ce6c
MadRobin1339 <114031799+MadRobin13@users.noreply.github.com>
2024-02-24 11:54:47 -0500

    ClimbCommand
    Added ClimbCommand for climbing to the trap. Also added constants for it.

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Crescendo2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 258;
-  public static final String GIT_SHA = "e4db8701144c5ac508f81458abeb066f903b4417";
-  public static final String GIT_DATE = "2024-02-24 11:31:40 EST";
+  public static final int GIT_REVISION = 263;
+  public static final String GIT_SHA = "3d67e80e6ccf80782d6c6be411f6425a854912eb";
+  public static final String GIT_DATE = "2024-02-24 11:39:32 EST";
   public static final String GIT_BRANCH = "assembly_commands";
-  public static final String BUILD_DATE = "2024-02-24 11:36:44 EST";
-  public static final long BUILD_UNIX_TIME = 1708792604969L;
+  public static final String BUILD_DATE = "2024-02-24 11:54:07 EST";
+  public static final long BUILD_UNIX_TIME = 1708793647135L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
---
+++ src/main/java/frc/robot/commands/climber/ClimbCommand.java
@@ -0,0 +1,50 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.climber;
+
+import java.lang.constant.Constable;
+import java.lang.invoke.ConstantBootstraps;
+
+import edu.wpi.first.hal.simulation.ConstBufferCallback;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
+import frc.robot.subsystems.ClimberSubsystem;
+
+public class ClimbCommand extends Command {
+
+    private ClimberSubsystem climb;
+
+    public ClimbCommand(ClimberSubsystem climb) {
+        this.climb = climb;
+        addRequirements(climb);
+  }
+
+  // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+      climb.stopClimber();
+      climb.setIsSafe(false);
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+      if (climb.getIsExtended())
+        climb.goToPosition(Constants.ClimbConstants.CLIMBER_ENCODER_CLIMBED_HEIGHT_IN_ROTATIONS, Constants.ClimbConstants.LEAD_CLIMBER_MOTOR__SPEED);
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+      climb.stopClimber();
+      climb.setIsExtended(true);
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+      return false;
+    }
+}
