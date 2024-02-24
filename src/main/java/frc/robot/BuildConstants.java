6f2a1b48739db2e5e5ca0349895d119750cd6a80
MadRobin1339 <114031799+MadRobin13@users.noreply.github.com>
2024-02-24 15:11:55 -0500

    ACP Subsystem Preferences
    Had to reapply changes because changes had to be pulled from origin. Still builds properly but is untested.

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Crescendo2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 271;
-  public static final String GIT_SHA = "d8af8031da1441f71c46d17a6aae174a4c5187d4";
-  public static final String GIT_DATE = "2024-02-24 14:11:27 EST";
+  public static final int GIT_REVISION = 274;
+  public static final String GIT_SHA = "83c4cd7add8031d5e0432ecb65250d8c3b012484";
+  public static final String GIT_DATE = "2024-02-24 15:06:54 EST";
   public static final String GIT_BRANCH = "assembly_commands";
-  public static final String BUILD_DATE = "2024-02-24 14:29:50 EST";
-  public static final long BUILD_UNIX_TIME = 1708802990488L;
+  public static final String BUILD_DATE = "2024-02-24 15:10:16 EST";
+  public static final long BUILD_UNIX_TIME = 1708805416052L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
