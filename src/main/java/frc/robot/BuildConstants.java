1e4c4b733b9b6614533f3632e0ecf04183645bf8
blaze <Arnnav0kudale@gmail.com>
2024-02-23 19:45:30 -0500

    reverted naming

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Crescendo2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 242;
-  public static final String GIT_SHA = "27693ce0b40b5eb2324a3dbcfdc077b663cdf0af";
-  public static final String GIT_DATE = "2024-02-23 19:25:53 EST";
+  public static final int GIT_REVISION = 243;
+  public static final String GIT_SHA = "dba5e8ed4400ff008672fb5bd99ec9922817f864";
+  public static final String GIT_DATE = "2024-02-23 19:29:27 EST";
   public static final String GIT_BRANCH = "assembly_commands";
-  public static final String BUILD_DATE = "2024-02-23 19:27:05 EST";
-  public static final long BUILD_UNIX_TIME = 1708734425113L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2024-02-23 19:40:04 EST";
+  public static final long BUILD_UNIX_TIME = 1708735204713L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
