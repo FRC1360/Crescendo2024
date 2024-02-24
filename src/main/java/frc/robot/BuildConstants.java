40e2394ae1f178fb4cb70f54c7012aa439a87b37
blaze <Arnnav0kudale@gmail.com>
2024-02-24 09:21:04 -0500

    fixing shintakepivot

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -4,16 +4,17 @@ package frc.robot;
  * Automatically generated file containing build version information.
  */
 public final class BuildConstants {
-  public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "Crescendo2024";
-  public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 243;
-  public static final String GIT_SHA = "dba5e8ed4400ff008672fb5bd99ec9922817f864";
-  public static final String GIT_DATE = "2024-02-23 19:29:27 EST";
-  public static final String GIT_BRANCH = "assembly_commands";
-  public static final String BUILD_DATE = "2024-02-23 19:40:04 EST";
-  public static final long BUILD_UNIX_TIME = 1708735204713L;
-  public static final int DIRTY = 0;
+	public static final String MAVEN_GROUP = "";
+	public static final String MAVEN_NAME = "Crescendo2024";
+	public static final String VERSION = "unspecified";
+	public static final int GIT_REVISION = 243;
+	public static final String GIT_SHA = "dba5e8ed4400ff008672fb5bd99ec9922817f864";
+	public static final String GIT_DATE = "2024-02-23 19:29:27 EST";
+	public static final String GIT_BRANCH = "assembly_commands";
+	public static final String BUILD_DATE = "2024-02-23 19:40:04 EST";
+	public static final long BUILD_UNIX_TIME = 1708735204713L;
+	public static final int DIRTY = 0;
 
-  private BuildConstants(){}
+	private BuildConstants() {
+	}
 }
