cb014377ed37c2116fe6e8160fec253f04f5a426
blaze <Arnnav0kudale@gmail.com>
2024-02-24 09:25:48 -0500

    now it should work. ignore casing ig

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Crescendo2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 245;
-  public static final String GIT_SHA = "40e2394ae1f178fb4cb70f54c7012aa439a87b37";
-  public static final String GIT_DATE = "2024-02-24 09:21:04 EST";
+  public static final int GIT_REVISION = 246;
+  public static final String GIT_SHA = "c556437e39e2a8ae74ebc35037ea24060df9575d";
+  public static final String GIT_DATE = "2024-02-24 09:22:13 EST";
   public static final String GIT_BRANCH = "assembly_commands";
-  public static final String BUILD_DATE = "2024-02-24 09:22:11 EST";
-  public static final long BUILD_UNIX_TIME = 1708784531977L;
+  public static final String BUILD_DATE = "2024-02-24 09:25:29 EST";
+  public static final long BUILD_UNIX_TIME = 1708784729066L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
