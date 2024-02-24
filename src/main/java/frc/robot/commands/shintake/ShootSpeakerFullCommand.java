dba5e8ed4400ff008672fb5bd99ec9922817f864
MadRobin1340 <114031799+MadRobin13@users.noreply.github.com>
2024-02-23 19:29:27 -0500

    Git Had Problems with Shoot Full Cmd
    Re-commiting the ShootSpeakerFullCommand which has not been changed since last commit. Also fixed Shintake package naming again.

--- src/main/java/frc/robot/commands/shintake/ShootSpeakerFullCommand.java
+++ src/main/java/frc/robot/commands/shintake/ShootSpeakerFullCommand.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands.shintake;
+package frc.robot.commands.Shintake;
 
 import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
@@ -11,21 +11,21 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Constants;
 import frc.robot.subsystems.ShintakeSubsystem;
 
+
+
 public class ShootSpeakerFullCommand extends SequentialCommandGroup {
+  private ShintakeSubsystem shooter;
+  private CommandXboxController xboxController;
+  private double prev = shooter.getBackEncoder();
+  /** Creates a new ShootSpeakerFullCommand. */
+  public ShootSpeakerFullCommand(ShintakeSubsystem shooter) {
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
+    addCommands(new InstantCommand( () -> shooter.setVelocity(Constants.ShintakeConstants.TARGET_SHOOT_VELOCITY_SPEAKER , Constants.ShintakeConstants.TARGET_SHOOT_VELOCITY_SPEAKER))
+    .alongWith(new OutakeCommand(shooter) /*REPLACE OUTAKE COMMAND WITH GO TO POSITION COMMAND*/)
+    .onlyIf(() -> shooter.getShooterReady(prev)).andThen(new ShootSpeakerCommand(shooter)).onlyIf(xboxController.b())
+    );
+  }
 
-	private ShintakeSubsystem shooter;
-	private CommandXboxController xboxController;
-
-	/** Creates a new ShootSpeakerFullCommand. */
-	public ShootSpeakerFullCommand(ShintakeSubsystem shooter) {
-		// Add your commands in the addCommands() call, e.g.
-		// addCommands(new FooCommand(), new BarCommand());
-		addCommands(
-				new InstantCommand(() -> shooter.setVelocity(Constants.ShintakeConstants.TARGET_SHOOT_VELOCITY_SPEAKER,
-						Constants.ShintakeConstants.TARGET_SHOOT_VELOCITY_SPEAKER))
-						.alongWith(new OutakeCommand(shooter) /* REPLACE OUTAKE COMMAND WITH GO TO POSITION COMMAND */)
-						.onlyIf(() -> shooter.getShooterReady(shooter.getBackEncoder()))
-						.andThen(new ShootSpeakerCommand(shooter)).onlyIf(xboxController.b()));
-	}
 
 }
