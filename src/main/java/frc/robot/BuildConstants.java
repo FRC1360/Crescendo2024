6a3eb0d103f103e7b60a65dc20ba8228ae7e558f
MadRobin1339 <114031799+MadRobin13@users.noreply.github.com>
2024-02-25 14:53:42 -0500

    Code that builds
    This code builds so I am pushing this code to help others whose code does not build.

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Crescendo2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 282;
-  public static final String GIT_SHA = "3e9c8ee888500f5491d214643950bd1313b93ff1";
-  public static final String GIT_DATE = "2024-02-25 11:28:28 EST";
+  public static final int GIT_REVISION = 283;
+  public static final String GIT_SHA = "f83a13f6857e601cac2d503c479ab8f7e5f2f05d";
+  public static final String GIT_DATE = "2024-02-25 14:43:59 EST";
   public static final String GIT_BRANCH = "assembly_commands";
-  public static final String BUILD_DATE = "2024-02-25 14:43:10 EST";
-  public static final long BUILD_UNIX_TIME = 1708890190754L;
+  public static final String BUILD_DATE = "2024-02-25 14:51:26 EST";
+  public static final long BUILD_UNIX_TIME = 1708890686003L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
