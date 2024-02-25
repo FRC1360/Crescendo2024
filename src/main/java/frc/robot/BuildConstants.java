3cda8a5b8e1110143f47abdd6db3bf111b49a2e1
BrightLaw9 <46764863+BrightLaw9@users.noreply.github.com>
2024-02-25 16:15:50 -0500

    Fix deprecated

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Crescendo2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 283;
-  public static final String GIT_SHA = "f83a13f6857e601cac2d503c479ab8f7e5f2f05d";
-  public static final String GIT_DATE = "2024-02-25 14:43:59 EST";
+  public static final int GIT_REVISION = 284;
+  public static final String GIT_SHA = "6a3eb0d103f103e7b60a65dc20ba8228ae7e558f";
+  public static final String GIT_DATE = "2024-02-25 14:53:42 EST";
   public static final String GIT_BRANCH = "assembly_commands";
-  public static final String BUILD_DATE = "2024-02-25 14:51:26 EST";
-  public static final long BUILD_UNIX_TIME = 1708890686003L;
+  public static final String BUILD_DATE = "2024-02-25 16:13:06 EST";
+  public static final long BUILD_UNIX_TIME = 1708895586252L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
